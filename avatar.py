0e6b01831ac7613287a31a026707084cbcaa927a
Ana Mendes <62084446+Ana138@users.noreply.github.com>
2023-04-26 22:08:39 -0300

    Create avatar.py
    criando meu avatar

---
+++ avatar.py
@@ -0,0 +1,9 @@
+from fileinput import filename
+import pagan
+inpt = 'Python Developer'
+img = pagan.Avatar(inpt, pagan.SHA512)
+img.show()
+outpath = 'output/'
+filename = inpt
+img.save(outpath, filename)
+img.change('new input', pagan.SHA256)
